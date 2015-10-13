ed8440a3bf02dfc863ba64b7cda201fe09c76204
Stephan Dubuke <sdubuke@csumb.edu>
2015-10-13 09:28:08 -0700

    inheritance
    using java

---
+++ Book.java
@@ -0,0 +1,27 @@
+public class Book extends Product
+{
+    private String author;
+
+    public Book()
+    {
+        super();
+        author = "";
+        addCount();
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String toString()
+    {
+        return super.toString() +
+            "Author:      " + author + "\n";
+    }
+}
---
+++ Software.java
@@ -0,0 +1,28 @@
+
+public class Software extends Product
+{
+    private String version;
+
+    public Software()
+    {
+        super();
+        version = "";
+        addCount();
+    }
+
+    public void setVersion(String version)
+    {
+        this.version = version;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public String toString()
+    {
+        return super.toString() +
+            "Version:     " + version + "\n";
+    }
+}
